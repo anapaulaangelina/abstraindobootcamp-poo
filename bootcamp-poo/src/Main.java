3df291c6fa5dee7ec00d0cce00dcaf592195cd61
Ana Paula <anapaula.angelina@yahoo.com.br>
2024-06-06 16:51:30 -0300

    Progredir , Mentoria e Curso

--- bootcamp-poo/src/Main.java
+++ bootcamp-poo/src/Main.java
@@ -1,5 +1,7 @@
 
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 import java.time.LocalDate;
 
@@ -21,8 +23,38 @@ public class Main {
         mentoria.setDescricao("descricao mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        //System.out.println(curso1);
+        //System.out.println(curso2);
+        //System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descricao Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devAna = new Dev();
+        devAna.setNome("Ana");
+        devAna.inscreverBootcamp(bootcamp);
+
+        System.out.println("Conteúdos Inscritos Ana:" + devAna.getConteudosInscritos());
+        devAna.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Concluidos Ana:" + devAna.getConteudosConcluidos());
+        System.out.println("XP:" + devAna.calcularTotalXp());
+        System.out.println("----------");
+
+        Dev devRonnie = new Dev();
+        devRonnie.setNome("Ronnie");
+        devRonnie.inscreverBootcamp(bootcamp);
+
+        System.out.println("Conteudos Inscritos Ronnie:" + devRonnie.getConteudosInscritos());
+        devRonnie.progredir();
+        devRonnie.progredir();
+        devRonnie.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Concluidos Ronnie:" + devAna.getConteudosConcluidos());
+        System.out.println("XP:" + devRonnie.calcularTotalXp());
     }
+
 }
